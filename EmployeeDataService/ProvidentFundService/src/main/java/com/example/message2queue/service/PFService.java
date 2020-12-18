ee9d844a2568ab50602b1100cdb5b59ab5fcbc99
Ankita Solaskar <vjsolaskar1992@gmail.com>
2020-12-18 18:26:21 +0530

    Deleted wrongly committed files

--- EmployeeDataService/ProvidentFundService/src/main/java/com/example/message2queue/service/PFService.java
+++
@@ -1,8 +0,0 @@
-package com.example.message2queue.service;
-
-import com.example.message2queue.dto.PFRequestDto;
-
-public interface PFService {
-	public String pfDetails(PFRequestDto pfrequest);
-
-}
--- EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.java.employee.service;
-
-import com.java.employee.entity.Employee;
-
-public interface EmployeeService {
-	
-public Employee getEmployeeDetails(Long id);
-
-}
